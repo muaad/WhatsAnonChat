d3433a32972f664810453e96dab8fbc5c512e6bd
muaad <muadh24@gmail.com>
2015-04-02 23:45:05 +0300

    changed way of checking if profile is completely setup

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,8 @@ class Contact < ActiveRecord::Base
 	has_many :progress, dependent: :delete_all
   has_many :steps, through: :progress
 
+  # after_create :set_country
+
   # scope	:profile_incomplete, -> { where("username = NULL OR gender = NULL OR age = NULL") }
   scope :profile_incomplete, (lambda do |id|
     where("username = NULL OR gender = NULL OR age = NULL AND id = #{id}")
@@ -14,5 +16,16 @@ class Contact < ActiveRecord::Base
   	rescue Exception => e
   		
   	end
+  end
+
+  def profile_incomplete
+  	username.nil? || gender.nil? || age.nil?
+  end
+
+  def set_country phone_number
+  	country_code = Phony.format(phone_number).split(" ")[0].gsub("+", "")
+  	country = Country.find_all_by_country_code(country_code)[0][1]["name"]
+  	short_name = Country.find_all_by_country_code(country_code)[0][1]["alpha2"].downcase
+  	contact.country = country
   end
 end
