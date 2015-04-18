3472b703aca3e5ec60963fe2df68fbf4442c131d
muaad <muadh24@gmail.com>
2015-04-18 12:36:05 +0300

    added relationship between Message and Contact

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ActiveRecord::Base
   belongs_to :chat
-  # belongs_to :from, :class_name => "Contact"
-  # belongs_to :to, :class_name => "Contact"
+  belongs_to :from, :class_name => "Contact"
+  belongs_to :to, :class_name => "Contact"
 end
