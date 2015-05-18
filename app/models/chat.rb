ca59f3bfaa164bc11cf8d11d400a39f9f32946b9
muaad <muadh24@gmail.com>
2015-05-19 00:28:16 +0300

    Moved most of the chat functionality to model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -14,4 +14,55 @@ class Chat < ActiveRecord::Base
 		end
 		receiver
 	end
+
+	def self.process sender, username="", message
+		if !username.empty?
+			recipient = Contact.find_by(username: username)
+			if !recipient.nil?
+				chats = sender.chats_with(recipient)
+				if chats.empty?
+					sender.chats.update_all(active: false)
+					recipient.chats.update_all(active: false)
+					chat = Chat.find_or_create_by(contact_id: sender.id, friend_id: recipient.id)
+					Message.create! chat: chat, body: message, from: sender.id, to: recipient.id
+				else
+					sender.chats.update_all(active: false)
+					recipient.chats.update_all(active: false)
+					chat = chats.first
+					chat.active = true
+					chat.save!
+					Message.create! chat: chat, body: message, from: sender.id, to: recipient.id
+				end
+				if !recipient.opted_in
+					send_message params[:phone_number], "@#{recipient.username} has chosen to be invisible. You won't be able to chat with #{recipient.male ? 'him' : 'her'} unless #{recipient.male ? 'he' : 'she'} is visible."
+				else
+					send_message recipient.phone_number, "@#{sender.username} says:\n\n#{message}"
+					# Message.create! chat: chat, body: message, from: sender.id, to: recipient.id
+				end
+			else
+				send_message params[:phone_number], "There is no user with the username @#{username}. Send /spin to get someone to talk to or /friends to get a list of the people you have chat with."
+			end
+		else
+			active = sender.active_chats.first
+			last_chat = sender.last_chat
+			chat = active.nil?? last_chat : active
+			if !chat.nil?
+				recipient = chat.recipient(sender)
+				if !active.nil?
+					send_message recipient.phone_number, "@#{sender.username} says:\n\n#{message}"
+					Message.create! chat: chat, body: message, from: sender.id, to: recipient.id
+				else
+					send_message recipient.phone_number, "@#{sender.username} says:\n\n#{message}\n\nYou don't have an active chat with @#{sender.username}. To reply to @#{sender.username}, start your message with @#{sender.username}."
+					Message.create! chat: sender.last_chat, body: message, from: sender.id, to: recipient.id
+					send_message params[:phone_number], "Your last active chat was with @#{recipient.username} who has since started another chat with someone else. Don't worry. We have delivered your message to @#{recipient.username}. You can start your message with @#{recipient.username} just to be safe."
+				end
+			else
+				send_message params[:phone_number], "You are currently not chatting with anyone. Send /spin to find a random person to talk to. You can also search by gender. Send /search/male or /search/female. To find some help on how to chat on here, send /help/chat."
+			end
+		end
+	end
+
+	def send_message phone_number, message
+		WhatsApp.send_message phone_number, message
+	end
 end
