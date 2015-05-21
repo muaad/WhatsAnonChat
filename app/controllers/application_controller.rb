831490e2cd1a29b9471771931c18f85fcc1f8a77
muaad <muadh24@gmail.com>
2015-05-21 23:12:59 +0300

    Added method to generate a verification code for contact

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
 
+  helper_method :current_contact
+
+  def generate_verification_code
+	  cs = [*'0'..'9']
+	  6.times.map { cs.sample }.join.upcase
+	end
+
   private
   	def current_contact
   	  @current_contact ||= Contact.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
