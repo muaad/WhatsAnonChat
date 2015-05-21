2cd72e5af3fc37256f1309e7d223627335af2353
muaad <muadh24@gmail.com>
2015-05-21 17:36:09 +0300

    Update method sending images

--- lib/whatsapp.rb
+++ lib/whatsapp.rb
@@ -3,12 +3,12 @@ class WhatsApp
 		HTTParty.post("http://app.ongair.im/api/v1/base/send", body: {token: Rails.application.secrets.ongair_token, phone_number: phone_number, text: message, thread: true})
 	end
 
-	def self.send_image phone_number
-		url = "http://app.ongair.im/api/v1/base/send_image"
+	def self.send_image phone_number, image_url
 		token = Rails.application.secrets.ongair_token
+		url = "http://app.ongair.im/api/v1/base/send_image?token=#{token}"
 
-		image_url = "bottle.png"
-		response = HTTParty.post(url, body: { token: token,  phone_number: phone_number, image: image_url, thread: true }, debug_output: $stdout)
+		# image_url = "bottle.png"
+		response = HTTParty.post(url, body: { phone_number: phone_number, image: image_url, thread: true }, debug_output: $stdout)
 		sent = response.parsed_response["sent"]
 	end
 end
