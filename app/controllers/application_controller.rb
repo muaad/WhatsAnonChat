5913ba41c1570ef4aa956d976df825272b60fce5
muaad <muadh24@gmail.com>
2015-05-21 17:59:28 +0300

    Added method to get currently logged in Contact

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
+
+  private
+  	def current_contact
+  	  @current_contact ||= Contact.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
+  	end
 end
