31a4569c98e79e3aed285f7aa67d2cfb29b15e5c
muaad <muadh24@gmail.com>
2015-06-24 00:28:18 +0300

    Create hash_tags from broadcasts and associate them to braodcasts

--- app/models/broadcast.rb
+++ app/models/broadcast.rb
@@ -1,3 +1,14 @@
 class Broadcast < ActiveRecord::Base
+	after_create :create_hash_tags
   belongs_to :contact
+  has_many :broadcast_hash_tags
+  has_many :hash_tags, through: :broadcast_hash_tags
+
+  def create_hash_tags
+  	hash_tags = text.split(" ").select{|w| w.start_with?('#')}
+  	hash_tags.each do |h|
+  		ht = HashTag.find_or_create_by! name: h
+  		BroadcastHashTag.create! broadcast_id: id, hash_tag: ht
+  	end
+  end
 end
--- app/models/broadcast_hash_tag.rb
+++ app/models/broadcast_hash_tag.rb
@@ -1,2 +1,4 @@
 class BroadcastHashTag < ActiveRecord::Base
+	belongs_to :broadcast
+	belongs_to :hash_tag
 end
