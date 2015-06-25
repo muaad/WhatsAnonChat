49fc6067cab221584dbea0e0b52a4a2c5df1859f
muaad <muadh24@gmail.com>
2015-06-25 22:21:26 +0300

    Added way to get usernames from message

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,8 @@ class Message < ActiveRecord::Base
   belongs_to :chat
   # belongs_to :from, :class_name => "Contact"
   # belongs_to :to, :class_name => "Contact"
+
+  def self.get_usernames text
+  	text.split(" ").select{|t| t.start_with?("@")}
+  end
 end
