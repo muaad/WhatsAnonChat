8840db3a62cbb8f44078e76d0eb494cfac460149
Muaad <muadh24@gmail.com>
2015-11-25 18:11:39 +0300

    Fixed a problem with WhatsApp#send_message

--- lib/whatsapp.rb
+++ lib/whatsapp.rb
@@ -1,11 +1,11 @@
 class WhatsApp
 	def self.send_message phone_number, message
-		HTTParty.post("http://app.ongair.im/api/v1/base/send", body: {token: Rails.application.secrets.ongair_token, phone_number: phone_number, text: message, thread: true})
+		HTTParty.post("https://ongair.im/api/v1/base/send", body: {token: Rails.application.secrets.ongair_token, phone_number: phone_number, text: message, thread: true})
 	end
 
 	def self.send_image phone_number, image_url
 		token = Rails.application.secrets.ongair_token
-		url = "http://app.ongair.im/api/v1/base/send_image?token=#{token}"
+		url = "https://ongair.im/api/v1/base/send_image?token=#{token}"
 
 		# image_url = "bottle.png"
 		response = HTTParty.post(url, body: { phone_number: phone_number, image: image_url, thread: true }, debug_output: $stdout)
